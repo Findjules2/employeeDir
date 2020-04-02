1205063f153ef87daeaf4b417ef7f01ef1a5ba72
Julie Berryhill <juliesellsnc@gmail.com>
2020-04-02 09:54:37 -0400

    deleted extra content

--- src/components/Search/index.js
+++ src/components/Search/index.js
@@ -1,8 +1,4 @@
 import React from "react";
-// import "./App.css";
-// import API from "./utils/API";
-// import EmployeeWrapper from "./components/EmployeeWrapper";
-// import Navbar from "./components/Navbar";
 
 function Search(props) {
   const inputHandler = event => {
@@ -28,46 +24,3 @@ function Search(props) {
 }
 
 export default Search;
-
-// class Search extends React.Component {
-//     constructor() {
-//         super()
-//         this.state = {
-//             employees: employee_list,
-//             search: ""
-//         }
-//     };
-//     handleSearch = (event) =>{
-//         event.preventDefault();
-//         this.setState({
-//             employees: this.state.employees.filter(emp => emp.includes(this.state.search))
-//         })
-//     }
-//     _handleChange = e =>{
-//         const {value, name } = e.target;
-//         this.setState({
-//             [name]:value,
-//             employees: employee_list.filter(emp => emp.name.first.includes(this.state.search))
-//         })
-//     }
-// render() {
-//     return (
-//       <div>
-//         <section style={styles.sectionStyles} className="section">
-//           <form className="form-group mx-auto py-5" style={styles.formStyles} onSubmit={() => this.handleSearch} >
-//             <input
-//               className="form-control"
-//               type="search"
-//               placeholder="Search"
-//               aria-label="Search"
-//               name ="search"
-//               value={this.state.search}
-//               onChange={(e) => this._handleChange(e)}
-//             />
-//           </form>
-//         </section>
-//         <EmployeeList employees={this.state.employees}  />
-//       </div>
-//     );
-//   }
-// }
